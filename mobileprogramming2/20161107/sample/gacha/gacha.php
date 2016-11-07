709ea48448752a73618b7b4adddde91311cac38d
Makito Katsube <katsubemakito@gmail.com>
2016-11-07 15:13:00 +0900

    add gacha

---
+++ mobileprogramming2/20161107/sample/gacha/gacha.php
@@ -0,0 +1,32 @@
+<?php
+if(count($argv)===1){
+	$loop = 1;
+}
+else if( 1<=$argv[1] && $argv[1]<=10 ){
+	$loop = $argv[1];
+}
+else{
+	print "Error: Option is not intger\n";
+	exit();
+}
+
+$chara = [
+	  ['ID101','バハムート','SR']
+	, ['ID201','イフリート','R']
+	, ['ID202','シヴァ','R']
+	, ['ID203','タイタン','R']
+	, ['ID301','チョコボ','N']
+	, ['ID302','デブチョコボ','N']
+	, ['ID303','モーグリ','N']
+	, ['ID304','カーバンクル','N']
+	, ['ID305','ケットシー','N']
+	, ['ID306','サボテンダー','N']
+];
+
+
+for($i=0; $i<$loop; $i++){
+	$rand = mt_rand(0, count($chara)-1);
+	$cur  = $chara[$rand];
+	
+	print $i+1 . '回目 '.$cur[2].' '.$cur[1].' ('.$cur[0].")\n";
+}
---
+++ mobileprogramming2/20161107/sample/gacha/gacha2.php
@@ -0,0 +1,41 @@
+<?php
+if(count($argv)===1){
+	$loop = 1;
+}
+else if( 1<=$argv[1] && $argv[1]<=10 ){
+	$loop = $argv[1];
+}
+else{
+	print "Error: Option is not intger\n";
+	exit();
+}
+
+/*
+$chara = [
+	  ['ID101','バハムート','SR']
+	, ['ID201','イフリート','R']
+	, ['ID202','シヴァ','R']
+	, ['ID203','タイタン','R']
+	, ['ID301','チョコボ','N']
+	, ['ID302','デブチョコボ','N']
+	, ['ID303','モーグリ','N']
+	, ['ID304','カーバンクル','N']
+	, ['ID305','ケットシー','N']
+	, ['ID306','サボテンダー','N']
+];
+*/
+$chara = [];
+$fp = fopen('data.csv', 'r');
+while( ($data = fgets($fp)) !== false ){
+	 $data    = rtrim($data);
+	 $buff    = explode(',', $data);
+	 $chara[] = $buff;
+}
+fclose($fp);
+
+for($i=0; $i<$loop; $i++){
+	$rand = mt_rand(0, count($chara)-1);
+	$cur  = $chara[$rand];
+	
+	print $i+1 . '回目 '.$cur[2].' '.$cur[1].' ('.$cur[0].")\n";
+}
